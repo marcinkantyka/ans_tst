5ab9ec33b3357e3c292b3c6ea00590cc3d947b6c
Marcin Kantyka <46459826+marcinkantyka@users.noreply.github.com>
2021-03-22 10:26:35 +0100

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+while [ "$1" != "" ]; do
+    case $1 in
+        -e | --var )  shift
+                                to_be_printed=$1
+                                ;;   
+    esac
+    shift
+done
+
+echo " ..........."
+echo $to_be_printed
 echo " xx .. xx"
+echo " xx .. xx"
+echo " xx .. xx"
+echo " xx .. xx"
+
 cat templates/*
 
 echo " --- --- ---"
