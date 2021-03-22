76ad5e17845f4bcb23f5580976d286f0d8a2a17f
Marcin Kantyka <46459826+marcinkantyka@users.noreply.github.com>
2021-03-22 10:32:52 +0100

    Update go.sh

--- go.sh
+++ go.sh
@@ -4,13 +4,16 @@ while [ "$1" != "" ]; do
     case $1 in
         -e | --var )  shift
                                 to_be_printed=$1
+                                ;;  
+        -e | --var2 )  shift
+                                to_be_printed_2=$1
                                 ;;   
     esac
     shift
 done
 
 echo " ..........."
-echo $to_be_printed
+echo "$to_be_printed  .. to_be_printed_2  "
 echo " xx .. xx"
 echo " xx .. xx"
 echo " xx .. xx"
